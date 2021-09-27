50fc6a386e67a31279be40d034690622c30a6c82
Yoshua-chan <31251884+Yoshua-chan@users.noreply.github.com>
2021-09-27 17:26:17 +0200

    added font installation

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if [ -e /bin/apt ]; then
-  INSTALL="sudo apt install -y"
-else if [ -e /bin/pacman ]; then
-  INSTALL="sudo pacman -S"
+# Check if the system has sudo or doas
+if [ -e /bin/sudo ]; then
+  SUDO=sudo
+else if [ -e /bin/doas ]; then
+  SUDO=doas
 fi
 fi
 
+# Check what package manager the system has
+if [ -e /bin/apt ]; then
+  INSTALL="$SUDO apt install -y"
+else if [ -e /bin/pacman ]; then
+  INSTALL="$SUDO pacman -S --noconfirm"
+else if [ -e /bin/dnf ]; then
+  INSTALL="$SUDO dnf -y install"
+fi
+fi
+fi
 
 mkdir -p ~/.config ~/.cache ~/.local/share ~/.local/bin
 
@@ -30,6 +41,15 @@ build-essentials \
 firefox \
 wget \
 curl \
+exa \
+rsync \
+scp \
+sshd"
+
+CARGO_PACKAGES="bat \
+tldr \
+ncspot \
+tokei \
 exa"
 
 ###### INSTALL ALL PACKAGES #########
@@ -38,13 +58,12 @@ do
   $INSTALL $package
 done
 
-CARGO_PACKAGES="bat \
-tldr \
-ncspot \
-tokei \
-exa"
-
 cargo install $CARGO_PACKAGES
+
+
+
+
+
 
 ####### GET RID OF OLD BASH DOTFILES ########
 if [ -e ~/.bashrc ]; then
@@ -57,7 +76,33 @@ fi
 
 git clone --recursive "git@github.com:Yoshua-chan/dotfiles.git" ~/.dotfiles
 
+(cd ~/.dotfiles; stow */)
+
+
+
+
+
+
+
 ##### DOWNLOAD AND INSTALL FONTS #####
 FONTS=$(mktemp -d)
+
+mkdir $FONTS/fonts
+
 wget "https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip" -O "$FONTS/JetBrainsMono.zip"
 wget "https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip" -O "$FONTS/FiraCode.zip"
+wget "https://github.com/microsoft/cascadia-code/releases/download/v2108.26/CascadiaCode-2108.26.zip" -O "$FONTS/CascadiaCode.zip"
+
+for file in $FONTS/*.zip
+do
+  unzip $file -d "$FONTS/fonts/$(basename $file .zip)"
+done
+
+rm -r $FONTS/*.zip
+
+for dir in $FONTS/fonts/*
+do
+  $SUDO cp -r $dir /usr/share/fonts/truetype/
+done
+
+$SUDO fc-cache -fv
