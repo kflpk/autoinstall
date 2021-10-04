d58d66b13e19992c2da37558d64963d0797175a9
Yoshua-chan <filipek.kacper311@gmail.com>
2021-10-04 17:43:43 +0200

    fix

--- autoinstall.sh
+++ autoinstall.sh
@@ -73,9 +73,10 @@ else if [ -e /bin/dnf ]; then
   ./snap-fedora.sh
 else if [ -e /bin/pacman ]; then
   ./snap-arch.sh
-fi fi fi
+fi 
+fi 
+fi
 
-$(seq 0 x)
 for i in $(seq 0 $(( ${#SNAP_PACKAGES[@]}-1)) )
 do
   $SUDO snap install ${SNAP_PACKAGES[$i]}
