f08f336422dbdc72f007dd411172271ef0d1a856
Yoshua-chan <filipek.kacper311@gmail.com>
2021-10-08 23:12:51 +0200

    moved fonts to new file, changed github link from ssh to http

--- autoinstall.sh
+++ autoinstall.sh
@@ -96,7 +96,7 @@ if [ -e ~/.bash_profile ]; then
   mv ~/.bash_profile ~/.bash_profile.old
 fi
 
-git clone --recursive "git@github.com:Yoshua-chan/dotfiles.git" ~/.dotfiles
+git clone --recursive "https://github.com/Yoshua-chan/dotfiles" ~/.dotfiles
 
 (cd ~/.dotfiles; stow */)
 
@@ -105,29 +105,6 @@ git clone --recursive "git@github.com:Yoshua-chan/dotfiles.git" ~/.dotfiles
 
 
 
-
-##### DOWNLOAD AND INSTALL FONTS #####
-FONTS=$(mktemp -d)
-
-mkdir $FONTS/fonts
-
-wget "https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip" -O "$FONTS/JetBrainsMono.zip"
-wget "https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip" -O "$FONTS/FiraCode.zip"
-wget "https://github.com/microsoft/cascadia-code/releases/download/v2108.26/CascadiaCode-2108.26.zip" -O "$FONTS/CascadiaCode.zip"
-
-for file in $FONTS/*.zip
-do
-  unzip $file -d "$FONTS/fonts/$(basename $file .zip)"
-done
-
-rm -r $FONTS/*.zip
-
-for dir in $FONTS/fonts/*
-do
-  $SUDO cp -r $dir /usr/share/fonts/truetype/
-done
-
-$SUDO fc-cache -fv
 
 
 ##### INSATLL VIM-PLUG #######
---
+++ fonts.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##### DOWNLOAD AND INSTALL FONTS #####
+FONTS=$(mktemp -d)
+
+mkdir $FONTS/fonts
+
+wget "https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip" -O "$FONTS/JetBrainsMono.zip"
+wget "https://github.com/tonsky/FiraCode/releases/download/5.2/Fira_Code_v5.2.zip" -O "$FONTS/FiraCode.zip"
+wget "https://github.com/microsoft/cascadia-code/releases/download/v2108.26/CascadiaCode-2108.26.zip" -O "$FONTS/CascadiaCode.zip"
+
+for file in $FONTS/*.zip
+do
+  unzip $file -d "$FONTS/fonts/$(basename $file .zip)"
+done
+
+rm -r $FONTS/*.zip
+
+for dir in $FONTS/fonts/*
+do
+  $SUDO cp -r $dir /usr/share/fonts/truetype/
+done
+
+$SUDO fc-cache -fv
+
